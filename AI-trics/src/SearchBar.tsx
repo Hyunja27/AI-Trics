84b29f2ad1ef8ff8e52e0cc5b6fcf3f2b5883258
hyunja27 <123gk@naver.com>
2022-07-26 17:29:49 +0900

    :building_construction: 07/26_Reorganize_position_and_feature #2

--- AI-trics/src/SearchBar.tsx
+++ AI-trics/src/SearchBar.tsx
@@ -1,65 +1,91 @@
 import { styled } from "@stitches/react";
 import { useState } from "react";
-import data from './data.json';
+import data from './data/data.json';
+
+interface Data {
+  id: number;
+  name: string;
+}
 
 export default function SearchBar() {
-  const [autoComplete, setAutoComplete] = useState<string[]>([]);
-  let tmpList: string[] = []
+  const [autoComplete, setAutoComplete] = useState<Data[]>([]);
+  let tmpList: Data[] = []
 
   const handleText = (e:any) => {
     tmpList = [];
     setAutoComplete(tmpList);
     data.forEach(function (elem) {
       if (e.target.value && elem.name.startsWith(e.target.value)) {
-        tmpList.push(elem.name);
+        tmpList.push(elem);
         setAutoComplete(tmpList);
-      }
+      };
     });
   }
 
+  const handleClick = (e:any) => {
+    console.log(e);
+  }
+
   return (
-      <SearchBarContainer>
-        <InputZone type="text" onChange={handleText} />
-        { autoComplete.length === 0 ? null : (
-          <DropDownList>
-            {(autoComplete.map((elem) => {
-              return (
-                <DropDownItem>
-                  {elem}
-                </DropDownItem>
-              );
-            }))}
-          </DropDownList>
-        )}
-      </SearchBarContainer>
+      <PageContainer>
+        <SearchBarContainer>
+          <InputZone type="text" onChange={handleText} />
+          { autoComplete.length === 0 ? null : (
+            <DropDownList>
+              {(autoComplete.map((elem) => {
+                return (
+                  <DropDownItem key={elem.id} onClick={handleClick} >
+                    {elem.name}
+                  </DropDownItem>
+                );
+              }))}
+            </DropDownList>
+          )}
+        </SearchBarContainer>
+      </PageContainer>
 
   );
 }
 
-const SearchBarContainer = styled("div", {
+const PageContainer = styled("div", {
   height: "100vh",
   width: "100vw",
-  margin: "auto",
-  position: "relative",
   display: "flex",
   alignItems: "center",
   justifyContent: "center"
 });
 
+const SearchBarContainer = styled("div", {
+});
+
 const InputZone = styled("input", {
   height: "2em",
   width: "40em",
+  margin: "0",
+  padding: "0",
+  fontSize: "1.2em",
+  textIndent: "1em",
 });
 
 const DropDownList = styled("ul", {
-  height: "2em",
-  display: "absolute",
-  position: "relative",
+  width: "40em",
+  margin: "auto",
+  padding: "0",
+  position: "absolute",
   backgroundColor: "white",
   border: "1px solid rgba(0, 0, 0, 0.3)",
   boxShadow: "0 10px 10px rgb(0, 0, 0, 0.3)",
-  zIndex: "3"
+  listStyle: "none",
+  fontSize: "1.2em",
 });
 
 const DropDownItem = styled("li", {
+  padding: "0",
+  paddingTop: "1em",
+  paddingBottom: "1em",
+  "&:hover": {
+    background: "rgb(0, 0, 0, 0.05)",
+    boxShadow: "0 10px 10px rgb(0, 0, 0, 0.3)",
+  },
+  textIndent: "1em",
 });
