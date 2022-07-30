c6b522cc193e3b77a7894e918cd19e65d57e9164
hyunja27 <123gk@naver.com>
2022-07-30 18:32:50 +0900

    :sparkles: 07/30_Add_Onclick_Event_feature #2

--- AI-trics/src/SearchBar.tsx
+++ AI-trics/src/SearchBar.tsx
@@ -1,5 +1,5 @@
-import { styled } from "@stitches/react";
-import { useState } from "react";
+import { styled } from '@stitches/react';
+import { useState } from 'react';
 import data from './data/data.json';
 
 interface Data {
@@ -23,69 +23,69 @@ export default function SearchBar() {
   }
 
   const handleClick = (e:any) => {
-    console.log(e);
+    console.log(e.target.innerText);
+    window.open('https://www.google.com/search?q=' + e.target.innerText)
   }
 
   return (
-      <PageContainer>
-        <SearchBarContainer>
-          <InputZone type="text" onChange={handleText} />
-          { autoComplete.length === 0 ? null : (
-            <DropDownList>
-              {(autoComplete.map((elem) => {
-                return (
-                  <DropDownItem key={elem.id} onClick={handleClick} >
-                    {elem.name}
-                  </DropDownItem>
-                );
-              }))}
-            </DropDownList>
-          )}
-        </SearchBarContainer>
-      </PageContainer>
-
+    <PageContainer>
+      <SearchBarContainer>
+        <InputZone type='text' onChange={handleText} />
+        { autoComplete.length === 0 ? null : (
+          <DropDownList>
+            {(autoComplete.map((elem) => {
+              return (
+                <DropDownItem key={elem.id} onClick={handleClick} >
+                  {elem.name}
+                </DropDownItem>
+              );
+            }))}
+          </DropDownList>
+        )}
+      </SearchBarContainer>
+    </PageContainer>
   );
 }
 
-const PageContainer = styled("div", {
-  height: "100vh",
-  width: "100vw",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center"
+const PageContainer = styled('div', {
+  height: '100vh',
+  width: '100vw',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
 });
 
-const SearchBarContainer = styled("div", {
+const SearchBarContainer = styled('div', {
 });
 
-const InputZone = styled("input", {
-  height: "2em",
-  width: "40em",
-  margin: "0",
-  padding: "0",
-  fontSize: "1.2em",
-  textIndent: "1em",
+const InputZone = styled('input', {
+  height: '2em',
+  width: '40em',
+  margin: '0',
+  padding: '0',
+  fontSize: '1.2em',
+  textIndent: '1em',
 });
 
-const DropDownList = styled("ul", {
-  width: "40em",
-  margin: "auto",
-  padding: "0",
-  position: "absolute",
-  backgroundColor: "white",
-  border: "1px solid rgba(0, 0, 0, 0.3)",
-  boxShadow: "0 10px 10px rgb(0, 0, 0, 0.3)",
-  listStyle: "none",
-  fontSize: "1.2em",
+const DropDownList = styled('ul', {
+  width: '40em',
+  margin: 'auto',
+  padding: '0',
+  position: 'absolute',
+  backgroundColor: 'white',
+  border: '1px solid rgba(0, 0, 0, 0.3)',
+  boxShadow: '0 10px 10px rgb(0, 0, 0, 0.3)',
+  listStyle: 'none',
+  fontSize: '1.2em',
 });
 
-const DropDownItem = styled("li", {
-  padding: "0",
-  paddingTop: "1em",
-  paddingBottom: "1em",
-  "&:hover": {
-    background: "rgb(0, 0, 0, 0.05)",
-    boxShadow: "0 10px 10px rgb(0, 0, 0, 0.3)",
+const DropDownItem = styled('li', {
+  padding: '0',
+  paddingTop: '1em',
+  paddingBottom: '1em',
+  '&:hover': {
+    background: 'rgb(0, 0, 0, 0.05)',
+    boxShadow: '0 10px 10px rgb(0, 0, 0, 0.3)',
   },
-  textIndent: "1em",
+  textIndent: '1em',
 });
